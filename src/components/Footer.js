8d02367cf682d3ff54fd9be50b5c3c5427b0dc2d
Rahulgit3D2Y <109753648+Rahulgit3D2Y@users.noreply.github.com>
2023-01-28 09:41:49 +0530

    1

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer=container">
+        <p>rahul3d2y's Foodary © 2023</p>
+      </div>
+       
+    </div>
+  );
+}
+export default Footer;
---
+++ src/recipe.js
@@ -0,0 +1,46 @@
+export const my_recipe = {
+  recipe_1: {
+    img :"/img/chirashidon.jpg",
+    name: 'Chirashi Don',
+    preparation_time: '30 minutes',
+    servings: '2',
+    ingredients: [
+      '1/2 cup rice',
+      '1/2 cup water',
+      '2 eggs',
+      '300g of salmon, tuna and salmon roe',
+    ],
+    instructions: [
+      'Cook rice and water in a rice cooker',
+      'Fry eggs in a pan',
+      'Serve rice, eggs and fish on a plate',
+    ],
+ 
+  },
+  recipe_2: {
+    img :"/img/pumpkin pie.jpg",
+    name: 'Pumpkin Pie',
+    preparation_time: '25 minutes',
+    servings: '8',
+    ingredients: [
+      '1 (15 ounce) can pumpkin puree',
+      '1 (14 ounce) can EAGLE BRAND® Sweetened Condensed Milk',
+      '2 large eggs',
+      '1 teaspoon ground cinnamon',
+      '½ teaspoon ground ginger',
+      '½ teaspoon ground nutmeg',
+      '½ teaspoon salt',
+      '1 (9 inch) unbaked pie crust',
+    ],
+    instructions: [
+      'Gather all ingredients.',
+      'Preheat the oven to 425 degrees F (220 degrees C).',
+      'Whisk pumpkin puree, condensed milk, eggs, cinnamon, ginger, nutmeg, and salt together in a medium bowl until smooth.',
+      'Pour into crust',
+      'Bake in the preheated oven for 15 minutes.',
+      'Reduce oven temperature to 350 degrees F (175 degrees C) and continue baking until a knife inserted 1 inch from the crust comes out clean, 35 to 40 minutes',
+      'Let cool before serving.',
+    ],
+ 
+  },
+}
